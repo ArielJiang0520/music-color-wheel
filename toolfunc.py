46e2a6055443fbe46c88a9922348e7674eeeb884
Backup <backup@Mac-7.local>
2021-05-09 23:37:32 -0400

    add toolfunc.py

---
+++ toolfunc.py
@@ -0,0 +1,37 @@
+import pickle
+import pandas
+import math
+import heapq
+
+def getClosestSong(r, g, b, df):
+    minDis = float("inf")
+    minIdx = -1
+    for i in range(len(df)):
+        dis = math.sqrt((r - df['R'][i]) ** 2 + (g - df['G'][i]) ** 2 + (b - df['B'][i]) ** 2)
+        if dis < minDis:
+            minIdx = i
+            minDis = dis
+    # print(df.loc[minIdx])
+    return minIdx
+
+def getGenreTopN(genre, df, N):
+    mask = df.genre.apply(lambda x: genre in x)
+    top = df[mask].nlargest(N, ['popularity'])
+    # print(top)
+    return top.index.tolist()  
+
+def getArtistColor(artist, df):
+    r, g, b = 0, 0, 0
+    songs = df.loc[df.artist == artist]
+    leng = len(songs)
+    for i in range(leng):
+        r += songs['R'][i]
+        g += songs['G'][i]
+        b += songs['B'][i]
+    return (r//leng, g//leng, b//leng)
+
+if __name__ == "__main__":
+    color_df = pickle.load(open("color_df.p", "rb"))
+    print(getClosestSong(218, 116, 101, color_df))
+    print(getGenreTopN('british soul', color_df, 10))
+    print(getArtistColor('Adele', color_df))
