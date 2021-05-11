67cf0fc6650ec261a2e3a983be8fa0d41d2fec37
ArielJiang0520 <32921432+ArielJiang0520@users.noreply.github.com>
2021-05-10 22:40:22 -0400

    more functions

--- data.py
+++ data.py
@@ -45,6 +45,7 @@ class Dataset:
         """
         query = np.array([r, g, b]).reshape(1, -1)
         song_id = np.argsort(-cosine_similarity(query, self.CM).flatten())[0]
+
         return self.df.iloc[song_id].to_dict()
 
     def get_songs_for_genre(self, genre, top_n):
@@ -63,11 +64,35 @@ class Dataset:
             self.ARTISTS_TO_IDS[artist][:top_n]
         ].to_dict('records')
 
+    def get_average_color_for_artist(self, artist):
+        """ return the mean color of all the songs by this artist """
+        assert artist in self.ARTIST_POOL
+
+        a_df = self.df.iloc[self.ARTISTS_TO_IDS[artist]]
+        mean_color = (np.array(a_df[['R', 'G', 'B']].sum(axis=0)) / len(a_df)).astype(int)
+
+        return {'R': mean_color[0], 'G': mean_color[1], 'B': mean_color[2]}
+
+    def get_average_color_for_genre(self, genre):
+        """ return the mean color of this genre """
+        assert genre in self.GENRE_POOL
+
+        g_df = self.df.iloc[self.GENRES_TO_IDS[genre]]
+        mean_color = (np.array(g_df[['R', 'G', 'B']].sum(axis=0)) / len(g_df)).astype(int)
+
+        return {'R': mean_color[0], 'G': mean_color[1], 'B': mean_color[2]}
+    
 
 
 if __name__ == '__main__':
     color_df = pickle.load(open('color_df.p', 'rb'))
     db = Dataset(color_df)
+
+    print(db.get_closest_song(0, 255, 255))
+    """
+    {'artist': 'Rihanna', 'title': 'Bitch Better Have My Money', 'popularity': 76.0, 'R': 10, 'G': 198, 'B': 198, 'genre': ['barbadian pop', 'dance pop', 'pop', 'post-teen pop', 'urban contemporary']}
+    """
+    
     print(db.get_songs_for_genre('pop', 3))
     """
     [{'artist': 'Justin Bieber', 'title': 'Peaches', 'popularity': 100.0, 'R': 84, 'G': 126, 'B': 172, 'genre': ['canadian pop', 'pop', 'post-teen pop']}, 
@@ -80,4 +105,14 @@ if __name__ == '__main__':
     [{'artist': 'Adele', 'title': 'Someone Like You', 'popularity': 78.0, 'R': 133, 'G': 62, 'B': 213, 'genre': ['british soul', 'pop', 'pop soul', 'uk pop']}, 
     {'artist': 'Adele', 'title': 'Rolling in the Deep', 'popularity': 76.0, 'R': 193, 'G': 63, 'B': 177, 'genre': ['british soul', 'pop', 'pop soul', 'uk pop']}, 
     {'artist': 'Adele', 'title': 'Set Fire to the Rain', 'popularity': 75.0, 'R': 167, 'G': 116, 'B': 157, 'genre': ['british soul', 'pop', 'pop soul', 'uk pop']}]
+    """
+
+    print(db.get_average_color_for_artist('Adele'))
+    """
+    {'R': 145, 'G': 89, 'B': 172}
+    """
+
+    print(db.get_average_color_for_genre('pop'))
+    """
+    {'R': 144, 'G': 110, 'B': 147}
     """
